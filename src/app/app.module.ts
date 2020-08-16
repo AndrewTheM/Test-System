9e7681ba585b304ede5685426ee209ea51abe6c8
Andrew <andrewmakoviichuk@gmail.com>
2020-08-16 20:38:46 +0300

    Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { QuestionComponent } from './components/question/question.component';
 
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
 
 import { TestsPageComponent, 
           HomePageComponent,
           ContactsPageComponent,
           NotFoundComponent,
           TestComponent,
-          LoginComponent } from './components';
+          LoginComponent,
+          QuestionComponent } from './components';
   
 @NgModule({
   declarations: [
@@ -34,11 +38,16 @@ import { TestsPageComponent,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatRadioModule,
     MatCardModule,
     MatChipsModule,
     MatCheckboxModule,
-    HttpClientModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  hidePassword: boolean = true;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: "",
+      password: ""
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  changeVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  submitCredentials() {
+    
+  }
 }
